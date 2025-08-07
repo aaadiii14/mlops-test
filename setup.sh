6966296586b28b1a3b29595a0bd3efd4f916d475
aaadiii14 <107793756+aaadiii14@users.noreply.github.com>
2025-08-07 18:29:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@
 # <az_ml_install>
 az extension add -n ml -y
 # </az_ml_install>
-
+echo "Increasing Azure CLI HTTP timeout to avoid transient errors"
+export AZURE_HTTP_TIMEOUT=60
 # Use a daily build
 # az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.9.0-py3-none-any.whl --yes
 # remove ml extension if it is installed
